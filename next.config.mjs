b7b6b77c058c5fdfe120afcd96ccc3b6f04d3f95
Alexis Cerio <alexis111@orange.fr>
2024-03-27 18:26:55 +0100

    Add client api calls to server mint function

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,15 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
       domains: ['github.githubassets.com','xumm.app'],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'nftstorage.link',
+          port: '',
+          pathname: '**',
+        },
+      ],
     },
   };
   
   export default nextConfig;
-  
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,32 @@
 import { Inter } from "next/font/google";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [hash, setHash] = useState("");
+  const [ipfsUri, setIpfsUri] = useState("");
+  const [ipfsImg, setIpfsImg] = useState("");
 
   const apiCallMint = async () => {
-    const response = await axios.post("http://localhost:8080/api/v1/mint");
-    setHash(response.data);
-    console.log("Hash of the token:", response.data);
+    const response = await axios.post("http://localhost:8080/api/v1/mint", {}, {
+      params: {
+        user: "Username"
+      }    
+    });
+
+    setHash(response.data.Hash);
+    console.log("Hash of the token:", response.data.Hash);
+
+    const url = `https://nftstorage.link/ipfs/${response.data.Metadatas.ipnft}`;
+    setIpfsUri(url);
+    console.log("IPFS URI:", url);
+
+    const imgUrl = response.data.Image;
+    const imgUrlGood = imgUrl.replace("ipfs://", "https://nftstorage.link/ipfs/")
+    setIpfsImg(imgUrlGood);
   };
 
   return (
@@ -36,6 +52,20 @@ export default function Home() {
               </p>
             </div>
           )}
+            {ipfsUri ? (
+                <>
+                  <Image
+                      src={ipfsImg}
+                      alt="NFT Image"
+                      width={450}
+                      height={150}
+                      priority
+                  />
+                  <a target="_blank" rel="noopener noreferrer" href={ipfsUri}>Your Ipfs Link Here</a>
+                </>
+            ) : (
+              <a>Click and wait few seconds...</a>
+            )}
       </div>
     </main>
   );
