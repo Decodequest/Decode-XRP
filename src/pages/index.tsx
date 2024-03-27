fb005044ab616d5bc662d3f15b6e8372d41bf583
prathibhabilagi <prathibhabilagi12@gmail.com>
2024-03-27 23:24:29 +0530

    firebase setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import Container from "@/components/Container";
 import Head from "next/head";
+import db from "../../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
-export default function Home() {
+type Props = {
+  details: [];
+};
+
+export default function Home({ details }: Props) {
   return (
     <>
       <Head>
@@ -9,6 +15,32 @@ export default function Home() {
         <meta name="description" content="Modules" />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <div>
+        <h1>Get Firbase Details</h1>
+        <div>
+          {details.map((data: any, index: any) => {
+            console.log("data", data);
+
+            return (
+              <h1 key={index} className="text-black">
+                {data.Name}
+              </h1>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const idoCollectionRef = collection(db, "Decode");
+  const doc = await getDocs(idoCollectionRef);
+  const user_details = doc.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+  return {
+    props: {
+      details: user_details,
+    },
+  };
+};
